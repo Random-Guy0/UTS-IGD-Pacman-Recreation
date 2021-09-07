8b1abf1848138d7b6d91def61db47e234d7b356c
Amren Gill <amren.gill@gmail.com>
2021-09-08 02:13:35 +1000

    added code to the audio manager to handle audio playback at the start of the game and when a pellet is eaten

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioClip[] soundEffects;
+    [SerializeField] private AudioClip[] music;
+
+    [SerializeField] private AudioSource soundEffectPlayer;
+    [SerializeField] private AudioSource musicPlayer;
+
+    private bool eatingPellet = false;
+
+    private void Start()
     {
-        
+        musicPlayer.clip = music[0];
+        musicPlayer.Play();
+        Invoke("SetupAudioSources", music[0].length);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetupAudioSources()
     {
-        
+        musicPlayer.clip = music[1];
+        musicPlayer.loop = true;
+        musicPlayer.Play();
+
+        soundEffectPlayer.clip = soundEffects[0];
+        soundEffectPlayer.loop = true;
+        soundEffectPlayer.Play();
+    }
+
+    public IEnumerator EatPellet()
+    {
+        if (!eatingPellet)
+        {
+            soundEffectPlayer.clip = soundEffects[1];
+            soundEffectPlayer.volume = 1.0f;
+            soundEffectPlayer.loop = false;
+            soundEffectPlayer.Play();
+            eatingPellet = true;
+            yield return new WaitForSeconds(soundEffects[1].length);
+            eatingPellet = false;
+            soundEffectPlayer.clip = soundEffects[0];
+            soundEffectPlayer.volume = 0.25f;
+            soundEffectPlayer.loop = true;
+            soundEffectPlayer.Play();
+        }
     }
 }
