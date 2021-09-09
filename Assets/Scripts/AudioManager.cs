6bec9f0f2a921fbb70a1febf9168b3129d3cd452
Amren Gill <amren.gill@gmail.com>
2021-09-09 17:37:44 +1000

    filled the AudioManager with methods that provide basic functionality. they will be updated when the game is more complete

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,6 +11,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource musicPlayer;
 
     private bool eatingPellet = false;
+    private bool dead = false;
 
     private void Start()
     {
@@ -19,15 +20,24 @@ public class AudioManager : MonoBehaviour
         Invoke("SetupAudioSources", music[0].length);
     }
 
-    private void SetupAudioSources()
+    public void SetupAudioSources()
     {
-        musicPlayer.clip = music[1];
-        musicPlayer.loop = true;
-        musicPlayer.Play();
+        GhostNormalState();
 
-        soundEffectPlayer.clip = soundEffects[0];
-        soundEffectPlayer.loop = true;
-        soundEffectPlayer.Play();
+        PlayMoveSound();
+
+        dead = false;
+    }
+
+    private void PlayMoveSound()
+    {
+        if (!dead)
+        {
+            soundEffectPlayer.clip = soundEffects[0];
+            soundEffectPlayer.volume = 0.25f;
+            soundEffectPlayer.loop = true;
+            soundEffectPlayer.Play();
+        }
     }
 
     public IEnumerator EatPellet()
@@ -41,10 +51,45 @@ public class AudioManager : MonoBehaviour
             eatingPellet = true;
             yield return new WaitForSeconds(soundEffects[1].length);
             eatingPellet = false;
-            soundEffectPlayer.clip = soundEffects[0];
-            soundEffectPlayer.volume = 0.25f;
-            soundEffectPlayer.loop = true;
-            soundEffectPlayer.Play();
+            PlayMoveSound();
         }
+    }
+
+    public IEnumerator CollideWithWall()
+    {
+        soundEffectPlayer.clip = soundEffects[2];
+        soundEffectPlayer.loop = false;
+        soundEffectPlayer.Play();
+        yield return new WaitForSeconds(soundEffects[2].length);
+        PlayMoveSound();
+    }
+
+    public void PacmanDeath() //update this method when the game has actually been made
+    {
+        musicPlayer.Stop();
+        soundEffectPlayer.clip = soundEffects[3];
+        soundEffectPlayer.loop = false;
+        soundEffectPlayer.Play();
+    }
+
+    public void GhostNormalState()
+    {
+        musicPlayer.clip = music[1];
+        musicPlayer.loop = true;
+        musicPlayer.Play();
+    }
+
+    public void GhostScaredState()
+    {
+        musicPlayer.clip = music[2];
+        musicPlayer.loop = true;
+        musicPlayer.Play();
+    }
+
+    public void GhostDeadState()
+    {
+        musicPlayer.clip = music[3];
+        musicPlayer.loop = true;
+        musicPlayer.Play();
     }
 }
