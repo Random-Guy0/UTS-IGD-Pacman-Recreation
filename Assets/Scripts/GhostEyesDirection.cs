4d226ff04dbb540326479f3bfcdfb5c67faa3f56
Amren Gill <amren.gill@gmail.com>
2021-09-13 02:20:23 +1000

    began animating pacman and the ghosts

---
+++ Assets/Scripts/GhostEyesDirection.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEyesDirection : MonoBehaviour
+{
+    [SerializeField] private Sprite[] verticalEyeSprites;
+    [SerializeField] private Sprite[] horizontalEyeSprites;
+
+    [SerializeField] private SpriteRenderer eyes;
+
+    private Sprite[,] eyeSprites = new Sprite[3, 3];
+
+    private void Start()
+    {
+        eyeSprites[0, 1] = horizontalEyeSprites[0];
+        eyeSprites[2, 1] = horizontalEyeSprites[1];
+
+        eyeSprites[1, 0] = verticalEyeSprites[0];
+        eyeSprites[1, 2] = verticalEyeSprites[1];
+    }
+
+    public void SetEyeDirection(Vector2 direction)
+    {
+        direction.x += 1;
+        direction.y += 1;
+
+        eyes.sprite = eyeSprites[(int)direction.x, (int)direction.y];
+    }
+}
