f4a6b71fb1d909d1d87dc581d59d85a6fe098c12
Amren Gill <amren.gill@gmail.com>
2021-09-21 02:39:21 +1000

    finished animating the ghosts

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -2,23 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostController : MonoBehaviour //write subclasses for differing ghost behaviour
+public abstract class GhostController : MonoBehaviour //write subclasses for differing ghost behaviour
 {
     [SerializeField] private GhostEyesDirection eyes;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer rend;
+
+    [SerializeField] private float startingScaredTime;
+    [SerializeField] private float startingFlashingTime;
+
+    private bool isDead = false;
+
+    private Vector2 direction;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            Vector2 direction = move();
+            direction = CalculateDirection(direction);
             eyes.SetEyeDirection(direction);
+        }
+
+        if (collision.gameObject.CompareTag("GhostHouse") && isDead) 
+        {
+            rend.enabled = true;
+            isDead = false;
         }
     }
 
-    private Vector2 move()
+    protected abstract Vector2 CalculateDirection(Vector2 previousDirection);
+
+    public void PowerPelletCollected()
     {
-        return Vector2.left;
+        animator.SetTrigger("isScared");
+        CancelInvoke();
+        Invoke("FlashingState", startingScaredTime);
     }
 
+    public void FlashingState()
+    {
+        animator.SetTrigger("isFlashing");
+        Invoke("ReturnToNormal", startingFlashingTime);
+    }
 
+    public void ReturnToNormal()
+    {
+        animator.SetTrigger("returnToNormal");
+    }
+
+    public void Dead()
+    {
+        rend.enabled = false;
+        isDead = true;
+    }
 }
