06135c91226e064fc15cf68cce3874ed36c9d312
Amren Gill <amren.gill@gmail.com>
2021-09-26 03:37:20 +1000

    added some basic rotation for the walls

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,7 +7,7 @@ public class LevelGenerator : MonoBehaviour
 	[SerializeField] private GameObject manualLevel;
 
 	[SerializeField] private GameObject[] levelSprites;
-
+	
 	private int[,] levelMap =
 	{
 		{1,2,2,2,2,2,2,2,2,2,2,2,2,7},
@@ -30,5 +30,58 @@ public class LevelGenerator : MonoBehaviour
     private void Start()
     {
 		Destroy(manualLevel);
+		GameObject level = new GameObject("level");
+		level.transform.Translate(new Vector3(-13.5f, 14.0f));
+
+		int maxI = levelMap.GetLength(0);
+		int maxJ = levelMap.GetLength(1);
+
+		for (int i = 0; i < maxI; i++)
+        {
+			for(int j = 0; j < maxJ; j++)
+            {
+				int spriteInt = levelMap[i, j] - 1;
+				Vector3 position = new Vector3(j, -i);
+				if (spriteInt >= 0)
+				{
+					GameObject currentSprite = Instantiate(levelSprites[spriteInt], level.transform);
+					currentSprite.transform.Translate(position);
+					
+					switch(spriteInt)
+                    {
+						case 1:
+						case 3:
+							AlignWall(i, j, currentSprite, maxJ);
+							break;
+                    }
+				}
+			}
+        }
+    }
+
+	private void AlignWall(int i, int j, GameObject currentSprite, int maxJ)
+	{
+		int previousSprite = -1;
+		int nextSprite = -1;
+
+		if(j - 1 >= 0)
+        {
+			previousSprite = levelMap[i, j - 1];
+			Debug.Log(previousSprite);
+        }
+
+		if(j + 1 < maxJ)
+        {
+			nextSprite = levelMap[i, j + 1];
+			Debug.Log(nextSprite);
+        }
+
+        if ((previousSprite <= 4 && previousSprite > 0) || (nextSprite <= 4 && nextSprite > 0))
+        {
+			Debug.Log("hi");
+			Vector3 rotation = currentSprite.transform.eulerAngles;
+			rotation.z += 90.0f;
+			currentSprite.transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
