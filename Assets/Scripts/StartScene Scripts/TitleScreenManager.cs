f102adc200ada4ddb2b9e4e86bfe78b322055589
Amren Gill <amren.gill@gmail.com>
2021-11-04 00:33:16 +1100

    StartScene score and time text now display values loaded from PlayerPrefs

--- Assets/Scripts/StartScene Scripts/TitleScreenManager.cs
+++ Assets/Scripts/StartScene Scripts/TitleScreenManager.cs
@@ -1,10 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleScreenManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text highScoreTxt;
+    [SerializeField] private TMP_Text bestTimeTxt;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        float bestTime = PlayerPrefs.GetFloat("BestTime", 0.0f);
+
+        highScoreTxt.text = "High Score: " + highScore;
+        bestTimeTxt.text = "Best Time: " + TimeSpan.FromSeconds(bestTime).ToString(@"mm\:ss\:ff");
+    }
+
     public void LoadLevelOne()
     {
         LoadScene(1);
@@ -19,4 +33,9 @@ public class TitleScreenManager : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(index);
     }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }    
 }
