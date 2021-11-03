8b91af9ad39e2564ce885d207f29b81aa459450c
Amren Gill <amren.gill@gmail.com>
2021-11-03 19:36:24 +1100

    the timer now counts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,16 +13,26 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_Text ghostScaredCountdownText;
     [SerializeField] private AudioManager audioManager;
     [SerializeField] private GameObject[] lifeIndicators;
+    [SerializeField] private TMP_Text gameTimer;
 
     private int score;
     private int scaredCountdown;
     private int lives;
+    private bool gameStarted;
+    private float startTime;
 
     private void Start()
     {
         score = 0;
         scoreText.text = score.ToString();
         lives = 3;
+        gameStarted = false;
+        StartGame();
+    }
+
+    private void Update()
+    {
+        UpdateGameTimer();
     }
 
     public void AddScore(int points)
@@ -82,5 +93,19 @@ public class GameManager : MonoBehaviour
     {
         lives--;
         Destroy(lifeIndicators[lives]);
+    }
+
+    private void UpdateGameTimer()
+    {
+        if(gameStarted)
+        {
+            gameTimer.text = TimeSpan.FromSeconds(Time.time - startTime).ToString(@"mm\:ss\:ff");
+        }
+    }
+
+    private void StartGame()
+    {
+        gameStarted = true;
+        startTime = Time.time;
     }
 }
